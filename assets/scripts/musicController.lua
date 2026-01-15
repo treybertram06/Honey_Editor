2b4dd39e3fc4d928e93f9ed4628692d9e453b2e5
treybertram06 <treybertram06@gmail.com>
2026-01-14 19:33:05 -0800

    Removed line enabling depth test on opengl init, fixed audio example in musicController.lua

--- assets/scripts/musicController.lua
+++ assets/scripts/musicController.lua
@@ -1,23 +1,34 @@
 -- musicController.lua
 
 local audioSrc = nil
+local playing = false
+local time = 0.0
 
 function OnCreate()
     audioSrc = self:GetComponent("AudioSource")
 
     audioSrc.volume = 1.0
     audioSrc.pitch = 1.0
-    audioSrc.loop = true
-    audioSrc:play()
+    audioSrc.loop = false
 
     Honey.Log("Music Controller Initialized.")
 end
 
 function OnUpdate()
-
-    if Honey.IsKeyPressed(Key.Space) then
+    if Honey.IsKeyPressed(Key.Space) and not playing then
         audioSrc:play()
-        Honey.Log("Music Playing (Hopefully).")
+        Honey.Log("Music Playing.")
+        playing = true
+    end
+
+    if playing then
+        time = time + dt
+    end
+
+    if time >= 42.0 then
+        audioSrc:play_at(31.0)
+        time = 31.0
+        --Honey.Log("Music Looping.")
     end
 
 end
