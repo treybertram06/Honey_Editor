6968bff23a804774f90a61ed108e4006f4fca3eb
treybertram06 <treybertram06@gmail.com>
2026-05-13 18:51:09 -0700

    - Fixed bug where on_update_runtime did not update streamed assets - Fixed bug where prefabs containing child entities did not have their transforms properly updated after loading

--- assets/scripts/lightSpawner.lua
+++ assets/scripts/lightSpawner.lua
@@ -13,14 +13,21 @@ function OnCreate()
     end
 
     for i = 1, Properties.numberOfLights do
-        local light = Honey.InstantiatePrefab("SpawnedLight")
-        if light then
-            local transform = light:GetTransform()
+        local object
+
+        if i % 2 == 0 then
+            object = Honey.InstantiatePrefab("SpawnedLight")
+        else
+            object = Honey.InstantiatePrefab("Sphere")
+        end
+
+        if object then
+            local transform = object:GetTransform()
             transform.translation.x = spawner_transform.translation.x + Honey.Random(-Properties.maxDistance, Properties.maxDistance)
             transform.translation.y = spawner_transform.translation.y + Honey.Random(-Properties.maxDistance, Properties.maxDistance)
             transform.translation.z = spawner_transform.translation.z + Honey.Random(-Properties.maxDistance, Properties.maxDistance)
 
-            local point_light = light:GetComponent("PointLight")
+            local point_light = object:GetComponent("PointLight")
             if point_light then
                 point_light.color.x = Honey.Random(0.0, 1.0)
                 point_light.color.y = Honey.Random(0.0, 1.0)
